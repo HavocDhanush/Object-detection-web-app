fcaa63d73242a8fa1939eb31050319f57599f9b7
HavocDhanush <93704624+HavocDhanush@users.noreply.github.com>
2024-03-15 18:26:06 +1000

    Update main.js

--- main.js
+++ main.js
@@ -33,19 +33,6 @@ function draw() {
 
     }
 
-
-    fill("#FF0000");
-    text("Dog" , 45, 75);
-    noFill();
-    stroke("#FF0000");
-    rect(30, 60, 450, 350);
-
-    fill("#FF0000");
-    text("Cat" , 320, 120);
-    noFill();
-    stroke("#FF0000");
-    rect(300, 90, 270, 320);
-    
 }
 
 function modelLoaded() {
