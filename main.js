213d00d63e6663e85bbf36343c3bea2a64afb07a
HavocDhanush <93704624+HavocDhanush@users.noreply.github.com>
2024-03-15 18:24:18 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 status = "";
 img = "";
+objects = [];
 
 function preload() {
     img = loadImage('dog_cat.jpg');
@@ -16,6 +17,23 @@ function setup() {
 
 function draw() {
     image(img, 0, 0, 640, 420);
+    if (status != "") {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Object Detected";
+
+            fill ("#FF00000")
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15 , objects[i].y + 15);
+            noFill();
+            stroke("#FF00000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+
+
+
+    }
+
+
     fill("#FF0000");
     text("Dog" , 45, 75);
     noFill();
@@ -27,6 +45,7 @@ function draw() {
     noFill();
     stroke("#FF0000");
     rect(300, 90, 270, 320);
+    
 }
 
 function modelLoaded() {
@@ -40,4 +59,6 @@ function gotResult(error, results) {
         console.log(error);
     }
     console.log(results);
+
+    objects = results;
 }
